597457c1e5681fada411632e70c176ca40d1b5d2
Elisabeth <elireyesgarcia8@gmail.com>
2025-04-01 23:08:40 -0400

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Wall_E_Compiler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
